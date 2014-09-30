f9894b0761f66258e24c7d01a4c4bb5b9849d537
Daniel Lienert <lienert@punkt.de>
2014-09-30 19:20:33 +0200

    ADD: Styles

---
+++ Resources/Private/config.rb
@@ -0,0 +1,11 @@
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../Public/Styles"
+sass_dir = "Styles"
+images_dir = "../Public/Images"
+javascripts_dir = "../Public/JavaScripts"
+
+output_style = :compressed
+
+add_import_path "bower_components"
