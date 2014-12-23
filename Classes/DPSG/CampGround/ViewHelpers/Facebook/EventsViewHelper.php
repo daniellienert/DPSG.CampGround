9d234e47543380968cafac34c0dc475f7380c64f
Daniel Lienert <daniel@lienert.cc>
2014-12-23 11:16:34 +0100

    [ADD] Events ViewHelper in teaser mode

---
+++ Classes/DPSG/CampGround/ViewHelpers/Facebook/EventsViewHelper.php
@@ -0,0 +1,13 @@
+<?php
+namespace DPSG\CampGround\ViewHelpers\Facebook;
+
+
+
+class EventsViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper {
+
+	public function render() {
+
+		return 'testVH';
+	}
+
+}
