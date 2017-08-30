6c872d2553014cafcf8c40679f40d836ded6a414
Daniel Lienert <daniel@lienert.cc>
2017-08-30 08:27:20 +0200

    TASK: Change Inject to InjectConfiguration and adapt code style to psr-2

--- Classes/DPSG/CampGround/ViewHelpers/Facebook/EventsViewHelper.php
+++ Classes/DPSG/CampGround/ViewHelpers/Facebook/EventsViewHelper.php
@@ -1,121 +1,126 @@
 <?php
+
 namespace DPSG\CampGround\ViewHelpers\Facebook;
 
 use Neos\Flow\Annotations as Flow;
 
-class EventsViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractTagBasedViewHelper {
+class EventsViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractTagBasedViewHelper
+{
 
 
-	/**
-	 * @Flow\Inject(setting="FacebookIntegration.PageId")
-	 * @var integer
-	 */
-	protected $pageId;
+    /**
+     * @Flow\InjectConfiguration(path="FacebookIntegration.PageId")
+     * @var integer
+     */
+    protected $pageId;
 
 
-	/**
-	 * @Flow\Inject(setting="FacebookIntegration.AccessToken")
-	 * @var string
-	 */
-	protected $accessToken;
+    /**
+     * @Flow\InjectConfiguration(path="FacebookIntegration.AccessToken")
+     * @var string
+     */
+    protected $accessToken;
 
 
-	/**
-	 * @var Boolean
-	 */
-	protected $configurationIsComplete = FALSE;
+    /**
+     * @var Boolean
+     */
+    protected $configurationIsComplete = FALSE;
 
 
-	/**
-	 * @var string
-	 */
-	protected $fields = 'id,name,description,location,venue,timezone,start_time,end_time,cover';
+    /**
+     * @var string
+     */
+    protected $fields = 'id,name,description,location,venue,timezone,start_time,end_time,cover';
 
 
-	public function initializeObject() {
-		$this->configurationIsComplete = $this->pageId != '' && $this->accessToken != '';
-	}
+    public function initializeObject()
+    {
+        $this->configurationIsComplete = $this->pageId != '' && $this->accessToken != '';
+    }
 
 
-	/**
-	 * @param integer $sinceDays
-	 * @param integer $untilDays
-	 * @param integer $maxEvents
-	 * @param string $coverType
-	 * @return string
-	 */
-	public function render($sinceDays = 7, $untilDays = 365, $maxEvents = 0, $coverType = 'square') {
+    /**
+     * @param integer $sinceDays
+     * @param integer $untilDays
+     * @param integer $maxEvents
+     * @param string $coverType
+     * @return string
+     */
+    public function render($sinceDays = 7, $untilDays = 365, $maxEvents = 0, $coverType = 'square')
+    {
 
 
-		if(!$this->configurationIsComplete) {
-			return 'Bitte trage die Facebook Page Id und dein AceessToken in die Settings ein.';
-		}
+        if (!$this->configurationIsComplete) {
+            return 'Bitte trage die Facebook Page Id und dein AceessToken in die Settings ein.';
+        }
 
-		$events = $this->loadEvents($sinceDays, $untilDays, $maxEvents);
+        $events = $this->loadEvents($sinceDays, $untilDays, $maxEvents);
 
-		$output = '';
-		$month = '';
-		$year = '';
+        $output = '';
+        $month = '';
+        $year = '';
 
-		foreach($events as $event) {
+        foreach ($events as $event) {
 
-			$event['url'] = sprintf('https://www.facebook.com/events/%s/', $event['id']);
+            $event['url'] = sprintf('https://www.facebook.com/events/%s/', $event['id']);
 
-			$date = new \DateTime($event['start_time']);
+            $date = new \DateTime($event['start_time']);
 
-			if($month !== $date->format('d')) {
-				$month = $date->format('d');
-				$this->templateVariableContainer->add('newMonth', TRUE);
-			}
+            if ($month !== $date->format('d')) {
+                $month = $date->format('d');
+                $this->templateVariableContainer->add('newMonth', TRUE);
+            }
 
-			if($year !== $date->format('Y')) {
-				$year = $date->format('Y');
-				$this->templateVariableContainer->add('newYear', TRUE);
-			}
+            if ($year !== $date->format('Y')) {
+                $year = $date->format('Y');
+                $this->templateVariableContainer->add('newYear', TRUE);
+            }
 
-			if($coverType === 'source') {
-				$event['cover']['url'] = $event['cover']['source'];
-			} else {
-				$event['cover']['url'] = sprintf('https://graph.facebook.com/%s/picture?type=%s', $event['id'], $coverType);
-			}
+            if ($coverType === 'source') {
+                $event['cover']['url'] = $event['cover']['source'];
+            } else {
+                $event['cover']['url'] = sprintf('https://graph.facebook.com/%s/picture?type=%s', $event['id'], $coverType);
+            }
 
-			$this->templateVariableContainer->add('event', $event);
-			$output .= $this->renderChildren();
-			$this->templateVariableContainer->remove('event');
+            $this->templateVariableContainer->add('event', $event);
+            $output .= $this->renderChildren();
+            $this->templateVariableContainer->remove('event');
 
-			if($this->templateVariableContainer->exists('newMonth')) $this->templateVariableContainer->remove('newMonth');
-			if($this->templateVariableContainer->exists('newYear')) $this->templateVariableContainer->remove('newYear');
-		}
+            if ($this->templateVariableContainer->exists('newMonth')) $this->templateVariableContainer->remove('newMonth');
+            if ($this->templateVariableContainer->exists('newYear')) $this->templateVariableContainer->remove('newYear');
+        }
 
-		return $output;
-	}
-
-
-	/**
-	 * @param $sinceDays
-	 * @param $untilDays
-	 * @param $maxEvents
-	 * @return array
-	 */
-	protected function loadEvents($sinceDays, $untilDays, $maxEvents) {
-		$sinceTime = time() - ($sinceDays * 86400);
-		$untilTime = time() + ($untilDays * 86400);
-
-		$json_link = sprintf('https://graph.facebook.com/v2.2/%s/events/?fields=%s&access_token=%s&since=%s&until=%s',
-			$this->pageId, $this->fields, $this->accessToken, $sinceTime, $untilTime
-		);
-
-		$jsonData = file_get_contents($json_link);
+        return $output;
+    }
 
 
-		$data = json_decode($jsonData, TRUE);
-		$events = $data['data'];
-		krsort($events);
+    /**
+     * @param $sinceDays
+     * @param $untilDays
+     * @param $maxEvents
+     * @return array
+     */
+    protected function loadEvents($sinceDays, $untilDays, $maxEvents)
+    {
+        $sinceTime = time() - ($sinceDays * 86400);
+        $untilTime = time() + ($untilDays * 86400);
 
-		if ($maxEvents > 0) {
-			$events = array_slice($events, 0, $maxEvents);
-			return $events;
-		}
-		return $events;
-	}
+        $json_link = sprintf('https://graph.facebook.com/v2.2/%s/events/?fields=%s&access_token=%s&since=%s&until=%s',
+            $this->pageId, $this->fields, $this->accessToken, $sinceTime, $untilTime
+        );
+
+        $jsonData = file_get_contents($json_link);
+
+
+        $data = json_decode($jsonData, TRUE);
+        $events = $data['data'];
+        krsort($events);
+
+        if ($maxEvents > 0) {
+            $events = array_slice($events, 0, $maxEvents);
+            return $events;
+        }
+        return $events;
+    }
 }
--- Classes/DPSG/CampGround/ViewHelpers/MultiColumnViewHelper.php
+++ Classes/DPSG/CampGround/ViewHelpers/MultiColumnViewHelper.php
@@ -1,134 +1,138 @@
 <?php
+
 namespace DPSG\CampGround\ViewHelpers;
 
 use Neos\Flow\Annotations as Flow;
 
-class MultiColumnViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractTagBasedViewHelper {
+class MultiColumnViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractTagBasedViewHelper
+{
 
-	/**
-	 * @var string
-	 */
-	protected $gridSpan;
-
-
-	/**
-	 * @var integer
-	 */
-	protected $itemsPerColumn;
+    /**
+     * @var string
+     */
+    protected $gridSpan;
 
 
-	/**
-	 * @var integer
-	 */
-	protected $gridColumns;
+    /**
+     * @var integer
+     */
+    protected $itemsPerColumn;
 
 
-	/**
-	 * @param array $each
-	 * @param string $as
-	 * @param integer $columns
-	 * @param integer $itemsPerColumn
-	 * @param integer $columnWidth
-	 * @param string $rowTag
-	 * @param string $columnTag
-	 * @param string $direction
-	 *
-	 * @throws \Neos\FluidAdaptor\Exception
-	 * @return string
-	 */
-	public function render(array $each, $as, $columns = 0, $itemsPerColumn = 0, $columnWidth = 200, $rowTag = 'div', $columnTag = 'div', $direction = 'vertical') {
-		$itemCount = count($each);
-
-		if($columns !== 0 && $itemsPerColumn !==0 || $columns == 0 && $itemsPerColumn == 0) {
-			throw new \Neos\FluidAdaptor\Exception('You have to define either $columns or $itemsPerColumn', 1421582949);
-		}
-
-		if($columns > 0) {
-			$this->calculateBootstrapGrids($columns);
-			$this->itemsPerColumn = ceil($itemCount / $this->gridColumns);
-		} else {
-			$this->itemsPerColumn = $itemsPerColumn;
-			$columns = ceil($itemCount / $itemsPerColumn);
-			$this->calculateBootstrapGrids($columns);
-		}
-
-		$content = '';
-		$itemIterator = 0;
-		$newRow = TRUE;
-		$newColumn = TRUE;
-		$firstRow = TRUE;
+    /**
+     * @var integer
+     */
+    protected $gridColumns;
 
 
-		foreach($each as $item) {
-			if (!$firstRow) {
-				if ($direction === 'vertical') {
-					$newRow = FALSE;
-					$newColumn = $itemIterator % $this->itemsPerColumn === 0 ? TRUE : FALSE;
-				} else {
-					$newColumn = TRUE;
-					$newRow = $itemIterator % $this->gridColumns === 0 ? TRUE : FALSE;
-				}
-			}
+    /**
+     * @param array $each
+     * @param string $as
+     * @param integer $columns
+     * @param integer $itemsPerColumn
+     * @param integer $columnWidth
+     * @param string $rowTag
+     * @param string $columnTag
+     * @param string $direction
+     *
+     * @throws \Neos\FluidAdaptor\Exception
+     * @return string
+     */
+    public function render(array $each, $as, $columns = 0, $itemsPerColumn = 0, $columnWidth = 200, $rowTag = 'div', $columnTag = 'div', $direction = 'vertical')
+    {
+        $itemCount = count($each);
 
-			if($newColumn && !$firstRow) $content .= sprintf('</%s>', $columnTag);
-			if($newRow && !$firstRow) $content .= sprintf('</%s>', $rowTag);
+        if ($columns !== 0 && $itemsPerColumn !== 0 || $columns == 0 && $itemsPerColumn == 0) {
+            throw new \Neos\FluidAdaptor\Exception('You have to define either $columns or $itemsPerColumn', 1421582949);
+        }
 
-			$this->templateVariableContainer->add('newRow', $newRow);
-			$this->templateVariableContainer->add($as, $item);
-			$this->templateVariableContainer->add('newColumn', $newColumn);
-			$this->templateVariableContainer->add('itemCount', $itemCount);
-			$this->templateVariableContainer->add('itemsPerColumn', $this->gridColumns);
-			$this->templateVariableContainer->add('gridSpan', $this->gridSpan);
-			$this->templateVariableContainer->add('containerWidth', $this->gridColumns * $columnWidth);
+        if ($columns > 0) {
+            $this->calculateBootstrapGrids($columns);
+            $this->itemsPerColumn = ceil($itemCount / $this->gridColumns);
+        } else {
+            $this->itemsPerColumn = $itemsPerColumn;
+            $columns = ceil($itemCount / $itemsPerColumn);
+            $this->calculateBootstrapGrids($columns);
+        }
 
-			$content .= $this->renderChildren();
-
-			$this->templateVariableContainer->remove($as);
-			$this->templateVariableContainer->remove('itemCount');
-			$this->templateVariableContainer->remove('gridSpan');
-			$this->templateVariableContainer->remove('containerWidth');
-			$this->templateVariableContainer->remove('itemsPerColumn');
-			$this->templateVariableContainer->remove('newRow');
-			$this->templateVariableContainer->remove('newColumn');
-
-			$itemIterator++;
-			$firstRow = FALSE;
-		}
-
-		$content .= sprintf('</%s>', $columnTag);
-		$content .= sprintf('</%s>', $rowTag);
-
-		return $content;
-	}
+        $content = '';
+        $itemIterator = 0;
+        $newRow = TRUE;
+        $newColumn = TRUE;
+        $firstRow = TRUE;
 
 
-	protected function calculateBootstrapGrids($columns) {
+        foreach ($each as $item) {
+            if (!$firstRow) {
+                if ($direction === 'vertical') {
+                    $newRow = FALSE;
+                    $newColumn = $itemIterator % $this->itemsPerColumn === 0 ? TRUE : FALSE;
+                } else {
+                    $newColumn = TRUE;
+                    $newRow = $itemIterator % $this->gridColumns === 0 ? TRUE : FALSE;
+                }
+            }
 
-		switch($columns) {
-			case 1:
-				$this->gridSpan = 12;
-				$this->gridColumns = 1;
-				break;
-			case 2:
-				$this->gridSpan = 6;
-				$this->gridColumns = 2;
-				break;
-			case 3:
-				$this->gridSpan = 4;
-				$this->gridColumns = 3;
-				break;
-			case 4:
-				$this->gridSpan = 3;
-				$this->gridColumns = 4;
-				break;
-			case 5:
-			case 6:
-				$this->gridSpan = 2;
-				$this->gridColumns = 6;
-				break;
-			default:
-				$this->gridSpan = 1;
-				$this->gridColumns = 12;
-		}
-	}
+            if ($newColumn && !$firstRow) $content .= sprintf('</%s>', $columnTag);
+            if ($newRow && !$firstRow) $content .= sprintf('</%s>', $rowTag);
+
+            $this->templateVariableContainer->add('newRow', $newRow);
+            $this->templateVariableContainer->add($as, $item);
+            $this->templateVariableContainer->add('newColumn', $newColumn);
+            $this->templateVariableContainer->add('itemCount', $itemCount);
+            $this->templateVariableContainer->add('itemsPerColumn', $this->gridColumns);
+            $this->templateVariableContainer->add('gridSpan', $this->gridSpan);
+            $this->templateVariableContainer->add('containerWidth', $this->gridColumns * $columnWidth);
+
+            $content .= $this->renderChildren();
+
+            $this->templateVariableContainer->remove($as);
+            $this->templateVariableContainer->remove('itemCount');
+            $this->templateVariableContainer->remove('gridSpan');
+            $this->templateVariableContainer->remove('containerWidth');
+            $this->templateVariableContainer->remove('itemsPerColumn');
+            $this->templateVariableContainer->remove('newRow');
+            $this->templateVariableContainer->remove('newColumn');
+
+            $itemIterator++;
+            $firstRow = FALSE;
+        }
+
+        $content .= sprintf('</%s>', $columnTag);
+        $content .= sprintf('</%s>', $rowTag);
+
+        return $content;
+    }
+
+
+    protected function calculateBootstrapGrids($columns)
+    {
+
+        switch ($columns) {
+            case 1:
+                $this->gridSpan = 12;
+                $this->gridColumns = 1;
+                break;
+            case 2:
+                $this->gridSpan = 6;
+                $this->gridColumns = 2;
+                break;
+            case 3:
+                $this->gridSpan = 4;
+                $this->gridColumns = 3;
+                break;
+            case 4:
+                $this->gridSpan = 3;
+                $this->gridColumns = 4;
+                break;
+            case 5:
+            case 6:
+                $this->gridSpan = 2;
+                $this->gridColumns = 6;
+                break;
+            default:
+                $this->gridSpan = 1;
+                $this->gridColumns = 12;
+        }
+    }
 }
